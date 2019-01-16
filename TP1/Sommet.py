ab59b1015c8a3ffcfbae6ffd442612b3d0afc9bf
Nicolas BRIET <fussure@gmail.com>
2019-01-16 10:38:56 +0100

    ca marche

--- TP1/Sommet.py
+++ TP1/Sommet.py
@@ -14,3 +14,17 @@ class Sommet:
         for precedent  in self.precedents:
             listPrecedent.append(precedent.nom)
         return f"{self.nom} : {self.couleur} , SUIV : {listSuivant} , PREC : {listPrecedent}"
+
+    def toStringNonOriente(self):
+        listSuivant = []
+        for suivant in self.suivants:
+            listSuivant.append(suivant.nom)
+        return f"{self.nom} : {self.couleur} , VOISINS : {listSuivant}"
+
+    @property
+    def degres(self):
+        return len(self.suivants) + len(self.precedents)
+
+    @property
+    def voisins(self):
+        return list(set(self.precedents+self.suivants))
--- TP1/main.py
+++ TP1/main.py
@@ -1,6 +1,11 @@
 from TP1.Sommet import Sommet
 from TP1.Graphe import Graphe
 
-g = Graphe(5)
-g.addArc(g.getSommet(1),g.getSommet(3))
+g = Graphe(5 , False)
+
+g.addArrete(g.getSommet(2),g.getSommet(4))
+g.addArrete(g.getSommet(1),g.getSommet(2))
+g.addArrete(g.getSommet(3),g.getSommet(4))
+g.addArrete(g.getSommet(0),g.getSommet(1))
+g.welshPowell()
 print(g)
